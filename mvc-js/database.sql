7fb9f641f20fa085a2430efc2c2544e70d31385c
Juan Carlos Condori Machicao <juanc.com@gmail.com>
2023-05-11 08:28:20 -0400

    :hammer: Adicionando sript de base de datos

---
+++ mvc-js/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+INSERT INTO items (name) VALUES ('Item 1');
+INSERT INTO items (name) VALUES ('Item 2');
+INSERT INTO items (name) VALUES ('Item 3');
