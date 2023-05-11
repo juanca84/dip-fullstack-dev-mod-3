dbab7943b4d09b41eb05b3b1234a1ab2744e69d4
Juan Carlos Condori Machicao <juanc.com@gmail.com>
2023-05-10 23:39:59 -0400

    :hammer: Actualización del proyecto mvc en js

--- mvc-js/main.js
+++ mvc-js/main.js
@@ -1,94 +1,107 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const { Pool } = require("pg");
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "mvc_db",
+  password: "postgres",
+  port: "54320",
+});
 
 // Modelo
-class TodoModel {
-  constructor() {
-    this.todos = [];
+class Model {
+  async getItems() {
+    const { rows } = await pool.query("select * from items;");
+    return rows;
   }
 
-  addTodo(todoText) {
-    const todo = {
-      text: todoText,
-      completed: false,
-    };
-    this.todos.push(todo);
-  }
-
-  editTodo(index, todoText) {
-    this.todos[index].text = todoText;
-  }
-
-  deleteTodo(index) {
-    this.todos.splice(index, 1);
-  }
-
-  toggleTodo(index) {
-    this.todos[index].completed = !this.todos[index].completed;
+  async addItem(name) {
+    await pool.query("insert into items (name) values ($1)", [name]);
   }
 }
 
-// Controlador
-class TodoController {
-  constructor(model) {
+//Vista
+class View {
+  render(data) {
+    let html = "";
+    for (let i = 0; i < data.length; i++) {
+      html += `
+      <li class="list-group-item">
+      <div class="row g-1">
+      <div class="col-sm-8">
+      ${data[i].name}
+      </div>
+      </div>
+      </li>`;
+    }
+    return `
+    <html>
+        <head>
+          <title>Items</title>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css
+          ">
+        </head>
+        <body>
+        <div class="container">
+          <div class="row g-1">
+          <div class="col-sm-4">
+          <h1>Items</h1>
+          <ul class="list-group">${html}</ul>
+          </br>
+          </br>
+          </div>
+          </div>
+          <form action="/add" method="post">
+          <div class="row g-2">
+          <div class="col-sm-4">
+            <input type="text" name="name" placeholder="Enter item name" class="form-control" aria-label="Sizing example input">
+          </div>
+          <div class="col-sm-4">
+            <button class="btn btn-primary" type="submit">Add Item</button>
+          </div>
+          </form>
+        </div>
+        </body>
+    </html>`;
+  }
+}
+
+//Controlador
+class Controller {
+  constructor(model, view) {
     this.model = model;
+    this.view = view;
   }
 
-  addTodo(todoText) {
-    this.model.addTodo(todoText);
+  async getItems(req, res) {
+    const data = await this.model.getItems();
+    const html = this.view.render(data);
+    res.send(html);
   }
 
-  editTodo(index, todoText) {
-    this.model.editTodo(index, todoText);
-  }
-
-  deleteTodo(index) {
-    this.model.deleteTodo(index);
-  }
-
-  toggleTodo(index) {
-    this.model.toggleTodo(index);
+  async addItem(req, res) {
+    const name = req.body.name;
+    await this.model.addItem(name);
+    const data = await this.model.getItems();
+    const html = this.view.render(data);
+    res.send(html);
   }
 }
 
-// Vistas (Rutas)
-const app = express();
-const todoModel = new TodoModel();
-const todoController = new TodoController(todoModel);
+//Instanciación
+const model = new Model();
+const view = new View();
+const controller = new Controller(model, view);
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/todos", (req, res) => {
-  res.send(todoController.model.todos);
-});
+app.get("/", controller.getItems.bind(controller));
+app.post("/add", controller.addItem.bind(controller));
 
-// Vistas (Rutas) (continuación)
-app.post("/todos", (req, res) => {
-  const todoText = req.body.text;
-  console.log(req.body)
-  todoController.addTodo(todoText);
-  res.sendStatus(200);
-});
-
-app.put("/todos/:index", (req, res) => {
-  const index = req.params.index;
-  const todoText = req.body.text;
-  todoController.editTodo(index, todoText);
-  res.sendStatus(200);
-});
-
-app.delete("/todos/:index", (req, res) => {
-  const index = req.params.index;
-  todoController.deleteTodo(index);
-  res.sendStatus(200);
-});
-
-app.patch("/todos/:index", (req, res) => {
-  const index = req.params.index;
-  todoController.toggleTodo(index);
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(port, () => {
+  console.log(`Este servidor se ejecuta en http://localhost:${port}`);
 });
